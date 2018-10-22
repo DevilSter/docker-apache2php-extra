d1abdb91a2b833f7e6e47be0cf2cccfe0173dced
Vyacheslav Tilikov <evangi.studio@hotmail.com>
2018-10-22 12:07:49 +0300

    7.1

--- build.sh
+++ build.sh
@@ -2,9 +2,11 @@
 
 sudo docker build -f Dockerfile --build-arg PHP_VER=7.0 -t devilster/apache2-php-extra:7.0 -t devilster/apache2-php-extra .
 sudo docker build -f Dockerfile --build-arg PHP_VER=7.2 -t devilster/apache2-php-extra:7.2 .
+sudo docker build -f Dockerfile --build-arg PHP_VER=7.1 -t devilster/apache2-php-extra:7.1 .
 sudo docker build -f Dockerfile --build-arg PHP_VER=5.6 -t devilster/apache2-php-extra:5.6 .
 sudo docker push devilster/apache2-php
 sudo docker push devilster/apache2-php:5.6
 sudo docker push devilster/apache2-php:7.0
 sudo docker push devilster/apache2-php:7.2
+sudo docker push devilster/apache2-php:7.1
 
